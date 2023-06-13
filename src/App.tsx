932d25ecf0d2506cb63752037bf4267e83f95ec5
alefsloan <alef_sloan@hotmail.com>
2023-06-13 16:31:00 -0300

    feat: create Intro Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css';
 import CoffeeCard from './Components/CoffeeCard';
 import Header from './Components/Header';
+import Intro from './Components/Intro';
 
 function App() {
   return (
     <div className="">
       <Header />
+      <Intro />
       <CoffeeCard />
     </div>
   )
---
+++ src/Components/Intro/index.tsx
@@ -0,0 +1,56 @@
+import { ClockCountdown, Coffee, Package, ShoppingCartSimple } from "@phosphor-icons/react";
+
+function Intro() {
+  return (
+    <div className="container max-w-[1120px] m-auto py-[92px] flex gap-14">
+      <div className="flex flex-col gap-[66px]">
+        <div className="flex flex-col gap-4">
+          <h1 className="font-baloo text-title-xl font-extrabold text-base-title m-0">
+            Encontre o café perfeito para qualquer hora do dia
+          </h1>
+          <p className="font-roboto text-text-lg text-base-subtitle">
+            Com o Coffee Delivery você recebeu seu café onde estiver, a qualquer
+            hora
+          </p>
+        </div>
+        <div className="flex flex-wrap gap-x-10 gap-y-5">
+          <span className="flex items-center gap-2">
+            <div className="p-2 rounded-full bg-yellow-dark">
+              <ShoppingCartSimple size={16} fill="white" weight="fill" />
+            </div>
+            <span className="text-text-md font-roboto text-base-text">
+              Compra simples e segura
+            </span>
+          </span>
+          <span className="flex items-center gap-2">
+            <div className="p-2 rounded-full bg-base-text">
+              <Package size={16} fill="white" weight="fill" />
+            </div>
+            <span className="text-text-md font-roboto text-base-text">
+              Compra simples e segura
+            </span>
+          </span>
+          <span className="flex items-center gap-2">
+            <div className="p-2 rounded-full bg-yellow-normal">
+              <ClockCountdown size={16} fill="white" weight="fill" />
+            </div>
+            <span className="text-text-md font-roboto text-base-text">
+              Compra simples e segura
+            </span>
+          </span>
+          <span className="flex items-center gap-2">
+            <div className="p-2 rounded-full bg-purple-normal">
+              <Coffee size={16} fill="white" weight="fill" />
+            </div>
+            <span className="text-text-md font-roboto text-base-text">
+              Compra simples e segura
+            </span>
+          </span>
+        </div>
+      </div>
+      <img className="h-[360px]" src="./public/images/Imagem.png" />
+    </div>
+  );
+}
+
+export default Intro;
