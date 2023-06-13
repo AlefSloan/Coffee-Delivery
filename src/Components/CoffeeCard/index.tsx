58d6d373e00bfe89ce5c0933cd672a0c026cd2e0
alefsloan <alef_sloan@hotmail.com>
2023-06-13 18:52:09 -0300

    feat: create coffeeMenuData and update CoffeeCard to receive coffee props

--- src/Components/CoffeeCard/index.tsx
+++ src/Components/CoffeeCard/index.tsx
@@ -1,41 +1,54 @@
 import { Minus, Plus, ShoppingCartSimple } from "@phosphor-icons/react";
 
-function CoffeeCard() {
+interface ICoffeeCardProps {
+  name: string,
+  description: string,
+  price: number,
+  imageUrl: string,
+  tags: string[],
+}
+
+function CoffeeCard({ coffee }: { coffee: ICoffeeCardProps }) {
+
   return (
     <div className="w-64 h-[310px] flex flex-col items-center bg-base-card rounded-tr-[2.25rem] rounded-bl-[2.25rem] rounded-tl-md rounded-br-md px-4">
-      <img className="-translate-y-5" src="/public/images/espresso.png" />
-      <div className="flex">
-        <span className="bg-yellow-light text-yellow-dark font-roboto text-tag font-bold px-2 py-1 rounded-full uppercase -translate-y-2">
-          Tradicional
-        </span>
+      <img className="-translate-y-5" src={coffee.imageUrl} />
+      <div className="flex gap-1">
+        {coffee.tags.map((tag) => (
+          <span key={tag} className="bg-yellow-light text-yellow-dark font-roboto text-tag font-bold px-2 py-1 rounded-full uppercase -translate-y-2">
+            {tag}
+          </span>
+        ))}
       </div>
       <div className="flex flex-col items-center translate-y-3 gap-2">
         <span className="text-title-sm font-bold text-base-subtitle font-baloo">
-          Expresso Tradicional
+          {coffee.name}
         </span>
         <span className="text-text-sm text-base-label font-roboto text-center">
-          O tradicional café feito com água quente e grãos moídos
+          {coffee.description}
         </span>
       </div>
       <div className="flex justify-evenly w-full translate-y-10">
         <span className="font-roboto text-text-sm text-base-text">
           R$
-          <span className="font-baloo text-title-md font-extrabold text-base-text">
-            9,90
+          <span className="font-baloo text-title-md font-extrabold text-base-text ml-1">
+            {coffee.price.toFixed(2)}
           </span>
         </span>
-        <div className="w-[72px] bg-base-button rounded-md flex items-center justify-around gap-1 p-2">
-          <button className="text-purple-dark">
-            <Minus size={14} />
-          </button>
-          <span className="text-white">0</span>
-          <button className="text-purple-dark">
-            <Plus size={14} />
+        <div className="flex gap-2">
+          <div className="w-[72px] bg-base-button rounded-md flex items-center justify-around gap-1 p-2">
+            <button className="text-purple-dark">
+              <Minus size={14} />
+            </button>
+            <span className="text-white">0</span>
+            <button className="text-purple-dark">
+              <Plus size={14} />
+            </button>
+          </div>
+          <button className="bg-purple-dark p-2 rounded-md">
+            <ShoppingCartSimple size={22} fill="white" weight="fill" />
           </button>
         </div>
-        <button className="bg-purple-dark p-2 rounded-md">
-          <ShoppingCartSimple size={22} fill="white" weight="fill" />
-        </button>
       </div>
     </div>
   );
--- src/Components/CoffeeContainer/index.tsx
+++ src/Components/CoffeeContainer/index.tsx
@@ -1,16 +1,127 @@
 import CoffeeCard from "../CoffeeCard";
 
+const coffeeMenuData = [
+  {
+    name: 'Expresso Tradicional',
+    description: 'O tradicional café feito com água quente e grãos moídos',
+    price: 9.9,
+    imageUrl: './public/images/espresso.png',
+    tags: ['Tradicional'],
+  },
+  {
+    name: 'Expresso Americano',
+    description: 'Expresso diluído, menos intenso que o tradicional',
+    price: 9.9,
+    imageUrl: './public/images/american.png',
+    tags: ['Tradicional'],
+  },
+  {
+    name: 'Expresso Cremoso',
+    description: 'Café expresso tradicional com espuma cremosa',
+    price: 9.9,
+    imageUrl: './public/images/creamy.png',
+    tags: ['Tradicional'],
+  },
+  {
+    name: 'Expresso Gelado',
+    description: 'Bebida preparada com café expresso e cubos de gelo',
+    price: 9.9,
+    imageUrl: './public/images/iced.png',
+    tags: ['Tradicional', 'Gelado'],
+  },
+  {
+    name: 'Café com Leite',
+    description: 'Meio a meio de expresso tradicional com leite vaporizado',
+    price: 9.9,
+    imageUrl: './public/images/milk.png',
+    tags: ['Tradicional', 'Com Leite'],
+  },
+  {
+    name: 'Latte',
+    description:
+      'Uma dose de café expresso com o dobro de leite e espuma cremosa',
+    price: 9.9,
+    imageUrl: './public/images/latte.png',
+    tags: ['Tradicional', 'Com Leite'],
+  },
+  {
+    name: 'Capuccino',
+    description:
+      'Bebida com canela feita de doses iguais de café, leite e espuma',
+    price: 9.9,
+    imageUrl: './public/images/capuccino.png',
+    tags: ['Tradicional', 'Com Leite'],
+  },
+  {
+    name: 'Macchiato',
+    description:
+      'Café expresso misturado com um pouco de leite quente e espuma',
+    price: 9.9,
+    imageUrl: './public/images/macchiato.png',
+    tags: ['Tradicional', 'Com Leite'],
+  },
+  {
+    name: 'Mocaccino',
+    description: 'Café expresso com calda de chocolate, pouco leite e espuma',
+    price: 9.9,
+    imageUrl: './public/images/mocaccino.png',
+    tags: ['Tradicional', 'Com Leite'],
+  },
+  {
+    name: 'Chocolate Quente',
+    description: 'Bebida feita com chocolate dissolvido no leite quente e café',
+    price: 9.9,
+    imageUrl: './public/images/chocolate.png',
+    tags: ['Especial', 'Com Leite'],
+  },
+  {
+    name: 'Cubano',
+    description:
+      'Drink gelado de café expresso com rum, creme de leite e hortelã',
+    price: 9.9,
+    imageUrl: './public/images/cuban.png',
+    tags: ['Especial', 'Alcoólico', 'Gelado'],
+  },
+  {
+    name: 'Havaiano',
+    description: 'Bebida adocicada preparada com café e leite de coco',
+    price: 9.9,
+    imageUrl: './public/images/havaian.png',
+    tags: ['Especial'],
+  },
+  {
+    name: 'Árabe',
+    description: 'Bebida preparada com grãos de café árabe e especiarias',
+    price: 9.9,
+    imageUrl: './public/images/arabian.png',
+    tags: ['Especial'],
+  },
+  {
+    name: 'Irlândes',
+    description: 'Bebida a base de café, uísque irlandês, açúcar e chantilly',
+    price: 9.9,
+    imageUrl: './public/images/irish.png',
+    tags: ['Especial', 'Alcoólico'],
+  },
+];
+
+
 function CoffeeContainer() {
   return (
     <div className="container max-w-[1120px] m-auto py-8">
-      <h2 className="font-baloo font-extrabold text-title-lg text-base-subtitle">Nossos cafés</h2>
+      <h2 className="font-baloo font-extrabold text-title-lg text-base-subtitle">
+        Nossos cafés
+      </h2>
       <div className="flex flex-wrap gap-x-8 gap-y-10 py-[54px]">
-        {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13].map((number) => (
-          <CoffeeCard key={number} />
+        {coffeeMenuData.map(({ name, description, price, imageUrl, tags }) => (
+          <CoffeeCard
+            key={name}
+            coffee={{ name, description, price, imageUrl, tags }}
+          />
         ))}
       </div>
     </div>
-  )
+  );
 }
 
 export default CoffeeContainer;
