adbccf623b2542a77939a1c23d8ec9f0862da464
alefsloan <alef_sloan@hotmail.com>
2023-06-19 22:21:24 -0300

    feat: create Checkout Page

--- src/Pages/Checkout/index.tsx
+++ src/Pages/Checkout/index.tsx
@@ -1,7 +1,27 @@
+import AddressForm from "../../Components/AddressForm";
+import OrderCheckout from "../../Components/OrderCheckout";
+import PaymentMethod from "../../Components/PaymentMethod";
+
 function Checkout() {
   return (
-    <div>
-      Checkout
+    <div className="container max-w-[1120px] m-auto flex gap-8">
+      <div>
+        <h2 className="font-baloo text-title-xs font-bold text-base-subtitle mb-[15px]">
+          Complete seu pedido
+        </h2>
+        <div className="flex flex-col gap-3">
+          <AddressForm />
+          <PaymentMethod />
+        </div>
+      </div>
+      <div className="w-full">
+        <h2 className="font-baloo text-title-xs font-bold text-base-subtitle mb-[15px]">
+          Cafés selecionados
+        </h2>
+        <div>
+          <OrderCheckout />
+        </div>
+      </div>
     </div>
   );
 }
